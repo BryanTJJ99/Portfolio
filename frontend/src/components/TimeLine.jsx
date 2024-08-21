520760074ba77f8a8584cbb2561852ca94fe2b79
Bryan Tan Jia Jun <bryan.tanjiajun@gmail.com>
2024-08-21 08:46:09 -0400

    biography page

---
+++ frontend/src/components/TimeLine.jsx
@@ -0,0 +1,91 @@
+import { Timeline, Typography } from "@material-tailwind/react";
+
+export default function TimeLine() {
+  return (
+    <Timeline color="secondary" orientation="vertical" className="text-left">
+      <Timeline.Item>
+        <Timeline.Header>
+          <Timeline.Separator />
+
+          <Timeline.Icon className="h-3 w-3" />
+        </Timeline.Header>
+
+        <Timeline.Body className="-translate-y-1.5">
+          <Typography color="default" className="font-bold">
+            Product Led Growth Intern, Workato
+          </Typography>
+
+          <Typography color="default" className="text-xs">
+            <b>May 2024 - Aug 2024 | Mountain View, California, US</b>
+          </Typography>
+          
+          <Typography type="small" className="mt-2 text-foreground">
+          During this internship, I embraced a solutions engineering role within the Documentation subteam, where my focus was on enhancing the efficiency and effectiveness of our technical writers' workflow.
+          <br />
+          <br />
+          One of the key projects I tackled was developing a metrics dashboard by integrating Google Analytics 4, Snowflake, and Sigma, and using Workato's low-code platform to build a seamless data pipeline. This enabled deeper insights into how customers and internal teams used our documentation.
+          <br />
+          <br />
+          I also enhanced our Continuous Integration (CI) process by integrating Reviewdog with automated linting tools, incorporating rules from Google and Microsoft style guides. This improvement streamlined code reviews and elevated documentation quality.
+          
+          <br />
+          <br />
+          <b>Learning Experiences:</b> I gained a deep understanding of the critical role product documentation plays in driving organic growth, especially in a late-stage growth company. It's often overlooked in tech companies, where the focus tends to be on product features and innovation. However, I learned that adopting a product-driven approach with well-structured documentation can significantly enhance user engagement and retention, acting as a catalyst for sustainable growth.
+
+          </Typography>
+        </Timeline.Body>
+      </Timeline.Item>
+
+      <Timeline.Item>
+        <Timeline.Header>
+          <Timeline.Separator />
+
+          <Timeline.Icon className="h-3 w-3" />
+        </Timeline.Header>
+
+        <Timeline.Body className="-translate-y-1.5">
+          <Typography color="default" className="font-bold">
+              Cross Border Acceleration & Innovation Intern, XNode 创极无限
+            </Typography>
+
+            <Typography color="default" className="text-xs">
+              <b>May 2023 - Aug 2023 | Shanghai, China</b>
+            </Typography>
+
+            <Typography type="small" className="mt-2 text-foreground">
+            During my internship in Shanghai's dynamic startup ecosystem, I collaborated with entrepreneurs, investors, and industry experts to drive innovation. A key project involved facilitating a Corporate Innovation Program for Scania, where I identified market opportunities in China's mega-city clusters, focusing on autonomous vehicles and battery recycling.
+            <br />
+            <br />
+            I also conducted market validation for startups from Singapore and Germany, helping them find strategic entry points into China’s tech landscape.            
+            <br />
+            <br />
+            <b>Learning Experiences:</b> Many questioned my decision to take on a non-engineering role for this overseas internship, expressing skepticism about how it would contribute to my career growth. However, I saw it as a unique opportunity to broaden my skill set and perspective. Embracing the challenge, I not only gained valuable insights into market dynamics but also immersed myself in the local culture, even taking on the lead role in a musical. This experience reinforced the importance of adaptability and cultural exchange, proving that stepping outside of traditional paths can lead to both professional and personal growth.
+            </Typography>
+        </Timeline.Body>
+      </Timeline.Item>
+
+      <Timeline.Item>
+        <Timeline.Header>
+          <Timeline.Icon className="h-3 w-3" />
+        </Timeline.Header>
+
+        <Timeline.Body className="-translate-y-1.5">
+          <Typography color="default" className="font-bold">
+              Data Engineer Intern, Unravel Carbon (Y-Combinator W22)
+            </Typography>
+
+            <Typography color="default" className="text-xs">
+              <b>May 2022 - Aug 2022 | Singapore</b>
+            </Typography>
+            
+            <Typography type="small" className="mt-2 text-foreground">
+            In this climate-tech start up, I initiated and led a project to optimize the ETL process by implementing an automated multi-label text classification system with BERT transformers. I integrated this with a React.js frontend and Flask backend. Additionally, I developed a tool for web and image scraping to gather datasets on scope 3 emissions, critical for completing emission inventories.
+            <br />
+            <br />
+            <b>Learning Experiences:</b> This was my first foray into the fast-paced environment of a seed-stage startup, where I quickly learned the importance of agility, innovation, and adaptability. The experience sparked a strong interest in the product side of technology, allowing me to see firsthand how ideas rapidly evolve into impactful solutions in a dynamic setting.
+            </Typography>
+        </Timeline.Body>
+      </Timeline.Item>
+    </Timeline>
+  );
+}
--- frontend/src/pages/BiographyPage.jsx
+++ frontend/src/pages/BiographyPage.jsx
@@ -1,11 +1,64 @@
 import React from 'react';
-
+import ProfilePhoto from '@/assets/images/ProfilePhoto.jpg';
+import TimeLine from '@/components/Timeline';
+import { Timeline, Typography } from "@material-tailwind/react";
 
 function BiographyPage() {
   return (
     <div>
-      {/* Your content goes here */}
+      <h1 className="text-2xl font-bold text-center">"I'm ready, I'm ready... to have fun!" — SpongeBob SquarePants</h1>
+
+      <div className="flex flex-col md:flex-row items-center space-x-5 mx-auto p-10" style={{ maxWidth: '800px' }}>
+        {/* Add the profile photo */}
+        <img src={ProfilePhoto} alt="Profile" className="mb-5 md:mb-0" style={{ width: '325px', borderRadius: '25%' }}/>
+
+        <div className="text-gray-700">
+          <p>
+          I’m a graduate student in the Accelerated Master’s Program between Singapore Management University (SMU) and Carnegie Mellon University (CMU), pursuing a Master’s in Information Systems Management at the Heinz College of Information Systems and Public Policy.
+          </p>
+          <br />
+          <p>
+          I am also a recipient of the IMDA Singapore Digital Scholarship. You can download my latest CV here.
+          </p>
+          <br />
+          <p>
+          Outside of work, I am an amateur street photographer with a passion for capturing the essence of urban life. My approach to visual storytelling is inspired by the works of Humans of New York and the Magnum Photos collective.
+          </p>
+        </div>
+      </div>
+
+      <hr className="my-8 border-gray-300 mx-auto" style={{ maxWidth: '800px' }} />
+
+      <div className="text-left mt-11 mx-auto" style={{ maxWidth: '800px' }}>
+  <h1 className="text-xl font-bold mb-5">About Me</h1>
+  <p>
+    Growing up in Singapore, I was always fascinated by how technology, grounded in first-principles thinking, could solve complex problems. When the COVID-19 pandemic disrupted my university experience, I saw it as an opportunity to hit pause after one semester—not as a setback, but as a chance to explore my passions, embrace new experiences, and gain clarity on my path forward.
+  </p>
+  <br />
+  <p>
+    During this break, I volunteered in my grassroots constituency, drafting appeal letters and helping set up an environmental charity. These experiences brought me closer to people, broadening my perspective and helping me see the interconnectedness of past and present. It was during this time that I realized my purpose in tech: to be an enabler, creating value-adding solutions that serve people’s needs.
+  </p>
+  <br />
+  <p>
+    With a renewed sense of purpose, I returned to my studies, focusing on how technology could be harnessed for greater societal impact. This journey led me to pursue a Master’s in Information Systems Management at CMU's Heinz College. Along the way, I sought out diverse and enriching experiences through internships across different countries and cultures. Each role deepened my approach to product development, encouraging me to think critically about both the "how" and the "why" behind every innovation.
+  </p>
+  <br />
+  <p>
+    Every step of my journey—from pausing my studies to embracing community service, to gaining global experience and having a bit of <b>FUN</b> along the way—has been about finding meaning and purpose in the work I do. I’ve learned that the most rewarding challenges are those that push us to grow, adapt, and enjoy the ride. As I continue to build my career, I’m eager to take on new challenges, collaborate with diverse teams, and create products that make a meaningful impact—while also making sure to have fun along the way.
+  </p>
+</div>
+
+
+
+      <hr className="my-8 border-gray-300 mx-auto" style={{ maxWidth: '800px' }} />
+
       
+
+      <div className="mx-auto" style={{ maxWidth: '800px' }}>
+      <h1 className="text-xl text-left font-bold mb-5">Work experiences</h1>
+        <TimeLine />
+      </div>
+
     </div>
   );
 }
