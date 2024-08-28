1a39604a86d54c0202408693a71638af89c43965
Bryan Tan Jia Jun <bryan.tanjiajun@gmail.com>
2024-08-28 18:03:46 -0400

    Update biography page with corrected organization name

--- frontend/src/pages/BiographyPage.jsx
+++ frontend/src/pages/BiographyPage.jsx
@@ -14,7 +14,7 @@ function BiographyPage() {
 
         <div className="text-gray-700">
           <p>
-          I’m a graduate student in the Accelerated Master’s Program in Carnegie Mellon University (CMU), pursuing a <a href="https://www.heinz.cmu.edu/programs/information-systems-management-master/" target="_blank" rel="noopener noreferrer" className="font-bold hover:underline">Master’s in Information Systems Management</a> at the Heinz College of Information Systems and Public Policy. I also serve as the current Masters Student Liaison for the Carnegie Mellon University Singapore Students’ Association.
+          I’m a graduate student in the Accelerated Master’s Program in Carnegie Mellon University (CMU), pursuing a <a href="https://www.heinz.cmu.edu/programs/information-systems-management-master/" target="_blank" rel="noopener noreferrer" className="font-bold hover:underline">Master’s in Information Systems Management</a> at the Heinz College of Information Systems and Public Policy. I also serve as the current Masters Student Liaison for the CMU Singapore Students’ Association.
           </p>
           <br />
           <p>
